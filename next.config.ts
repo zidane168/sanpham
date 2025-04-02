7246a84f70370b23577ede5a2b834be7ffc1e5c5
VILH-CORE-I5 <vi.lh@>
2025-04-02 22:10:22 +0700

    add next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+ 
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true, 
 };
 
 export default nextConfig;
